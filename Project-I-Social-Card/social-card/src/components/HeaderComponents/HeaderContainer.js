ddee6aae4bbfe262eb7192c3b1ef8b0eaf63197e
dwilley21 <daltonwilley@gmail.com>
2019-05-08 01:06:25 -0500

    initial changes to card component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+import './ImageThumbnail'
+import HeaderTitle from './HeaderTitle';
+
+
+const HeaderContainer = () => {
+   return <HeaderTitle />
+   
+}
+
+export default HeaderContainer 
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeadersContent = () => {
+    return <div>
+        <p>
+            Let's learn React by building simple interfaces with compoonets. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using componenets you are well on your way to mastering React!
+        </p>
+    </div>
+}
+
+export default HeadersContent
